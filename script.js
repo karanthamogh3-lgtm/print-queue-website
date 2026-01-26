b9fc87e85d19386a6ad5666f95f5957b17ec724b
karanthamogh3-lgtm <karanthamogh3@gmail.com>
2026-01-27 01:21:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,20 +32,31 @@ function addToQueue() {
 function loadDashboard() {
   const q = JSON.parse(localStorage.getItem("printQueue")) || [];
   const list = document.getElementById("orderList");
+  const people = document.getElementById("people");
+  const time = document.getElementById("time");
 
   list.innerHTML = "";
+
   q.forEach((order, index) => {
     list.innerHTML += `
       <li>Queue ${index + 1}: ${order.name} - ${order.copies} copies</li>
     `;
   });
+
+  people.innerText = q.length;
+  time.innerText = q.reduce((sum, o) => sum + o.copies, 0) * 2;
 }
+
 
 // OWNER: Clear queue
 function clearQueue() {
   localStorage.removeItem("printQueue");
-  loadDashboard();
+  document.getElementById("orderList").innerHTML = "";
+  document.getElementById("people").innerText = 0;
+  document.getElementById("time").innerText = 0;
 }
+
+
 
 
 
